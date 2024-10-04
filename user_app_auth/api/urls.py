79aa4f12834f9ec0f7885ead993fc86bf69a91ee
Sidharth Jain <sidharthjainp9@gmail.com>
2024-10-04 21:40:59 +0530

    logout- token deletion

--- user_app_auth/api/urls.py
+++ user_app_auth/api/urls.py
@@ -6,4 +6,6 @@ from . import views
 urlpatterns = [
     path('login/',obtain_auth_token,name='login'),
     path('register/',views.registration_view,name='register'),
+    path('logout/',views.logout_view,name='logout'),
+
 ]
