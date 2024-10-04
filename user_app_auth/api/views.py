afd276edecb08f2601ce2ca6805671c98443f11e
Sidharth Jain <sidharthjainp9@gmail.com>
2024-10-04 21:32:00 +0530

    token auth registraion 2

--- user_app_auth/api/views.py
+++ user_app_auth/api/views.py
@@ -1,11 +1,32 @@
 from rest_framework.decorators import api_view
-from .serializers import RegistrationSerializer
 from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
 
+from .serializers import RegistrationSerializer
+from user_app_auth import models
+
+
+@api_view(['POST',])
 def registration_view(request):
     
     if request.method=='POST':
         serializer = RegistrationSerializer(data=request.data)
+
+        #store everything inside it 
+        data = {}
+
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
+            account = serializer.save()
+            
+            # storing below data in dict format which is initialized empty at start
+            # will be seen in response after post rq for registration
+            data['response'] = "Registartion successful..."
+            data['username'] = account.username
+            data['email'] = account.email
+
+            token = Token.objects.get(user=account).key
+            data['token'] = token
+        else:
+            data = serializer.errors
+
+        return Response(data)
