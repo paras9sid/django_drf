83fc23534f43b48d19baad404a1ae3184767daf1
Sidharth Jain <sidharthjainp9@gmail.com>
2024-11-14 18:18:03 +0530

    JWT-registration set- finished

--- user_app_auth/api/views.py
+++ user_app_auth/api/views.py
@@ -2,9 +2,10 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from .serializers import RegistrationSerializer
-from user_app_auth import models
+# from user_app_auth import models
 
 
 @api_view(['POST',])
@@ -32,8 +33,15 @@ def registration_view(request):
             data['username'] = account.username
             data['email'] = account.email
 
-            token = Token.objects.get(user=account).key
-            data['token'] = token
+            # token = Token.objects.get(user=account).key
+            # data['token'] = token
+
+            refresh = RefreshToken.for_user(account)
+            data['token'] = {
+                                'refresh': str(refresh),
+                                'access': str(refresh.access_token),
+                            }
+
         else:
             data = serializer.errors
 
